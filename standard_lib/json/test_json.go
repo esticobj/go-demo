53b1af7ef5a3729123388e08f6199c9db0b820e5
lcr <746318202@qq.com>
2021-12-20 23:22:28 +0800

    添加json库demo

---
+++ standard_lib/json/test_json.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name  string
+	Age   int
+	Email string
+}
+
+func main() {
+	person := Person{
+		Name:  "tom",
+		Age:   19,
+		Email: "asdas@qq.com",
+	}
+	str, _ := json.Marshal(person) // 结构体中的元素必须为大写
+	fmt.Printf("json: %v\n", string(str))
+	var p Person
+	json.Unmarshal(str, &p) // 第二个参数为指针类型
+	fmt.Printf("p: %v\n", p)
+}
